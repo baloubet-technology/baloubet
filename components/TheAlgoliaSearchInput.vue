dc8a8d01086d1bf56648fc2bea1cb5cdbd531a3f
hugo mori <mori.hugo@outlook.com>
2020-11-28 19:53:04 +0100

    fix: change design input

--- components/TheAlgoliaSearchInput.vue
+++ components/TheAlgoliaSearchInput.vue
@@ -3,16 +3,8 @@
     <ais-instant-search-ssr>
       <div>
         <ais-search-box>
-          <div slot-scope="{ currentRefinement, isSearchStalled, refine }">
-            <input
-              type="search"
-              class="appearance-none min-w-0 w-full bg-white border border-gray-300 py-2 px-4 text-base leading-6 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline focus:placeholder-gray-400 transition duration-150 ease-in-out sm:max-w-xs"
-              v-model="currentRefinement"
-              @input="refine($event.currentTarget.value)"
-            >
-          </div>
 
-          <div slot-scope="{ currentRefinement, isSearchStalled, refine }" class="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
+         <div slot-scope="{ currentRefinement, isSearchStalled, refine }" class="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
             <div class="max-w-s w-full">
               <label for="search" class="sr-only">Search</label>
               <div class="relative">
@@ -22,7 +14,7 @@
                     <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                   </svg>
                 </div>
-                <input v-model="currentRefinement" @input="refine($event.currentTarget.value)" name="search" id="search" class="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-gray-900 focus:border-gray-900 sm:text-sm" placeholder="Search" type="search">
+                <input v-model="currentRefinement" @input="refine($event.currentTarget.value)" name="search" id="search" class="appearance-none min-w-0 w-full bg-white border border-gray-300 py-2 px-4 pl-10 pr-3 text-base leading-6 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline focus:placeholder-gray-400 transition duration-150 ease-in-out sm:max-w-xs" placeholder="Search" type="search">
               </div>
             </div>
           </div>
