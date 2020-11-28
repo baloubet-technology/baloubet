83f2eba037b28a2c73ce28e1f1be2f0555fdf90f
hugo mori <mori.hugo@outlook.com>
2020-11-28 10:13:35 +0100

    feat: change input search and display result

--- components/TheAlgoliaSearchInput.vue
+++ components/TheAlgoliaSearchInput.vue
@@ -11,6 +11,22 @@
               @input="refine($event.currentTarget.value)"
             >
           </div>
+
+          <div slot-scope="{ currentRefinement, isSearchStalled, refine }" class="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
+            <div class="max-w-s w-full">
+              <label for="search" class="sr-only">Search</label>
+              <div class="relative">
+                <div class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                  <!-- Heroicon name: search -->
+                  <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+                <input v-model="currentRefinement" @input="refine($event.currentTarget.value)" name="search" id="search" class="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-gray-900 focus:border-gray-900 sm:text-sm" placeholder="Search" type="search">
+              </div>
+            </div>
+          </div>
+
         </ais-search-box>
       </div>
 
@@ -28,10 +44,10 @@
                   <ais-hits>
 
                     <div slot="item" slot-scope="{ item }" class="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                      <div>
-                        <img :src="item.picture" class="h-6 w-6 rounded-full">
-                        <p class="text-base font-medium text-gray-900">
-                          {{ item.name }}
+                      <div class="flex items-center">
+                        <img :src="item.picture" class="h-8 w-8 rounded-md border-gray-300">
+                        <p class="text-base font-medium text-gray-900 ml-3">
+                          {{ limit(item.name, 20) }}
                         </p>
                       </div>
                     </div>
@@ -124,6 +140,12 @@
             instantSearchState: instantsearch.getState()
           }
         })
+    },
+    methods: {
+    limit(str, length) {
+      if (str.length < length) return str
+      return `${str.substr(0, length)}...`
     }
+  },
   }
 </script>
